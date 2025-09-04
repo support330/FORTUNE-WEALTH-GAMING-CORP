2a80eef312a5fbe9aede777c08781a781d3d5ced
support330 <support@fwgcorp.com>
2025-09-04 15:15:40 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",        // root HTML files
+    "./SRC/**/*.{html,js}"  // your project SRC folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
